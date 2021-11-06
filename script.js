9fea559dd0a4e6de307c908bb0f6d4f033f887a1
gynaa78 <gynaa78@yahoo.com>
2021-11-06 08:00:54 +0000

    300+

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
+//Cloned to use
 //You can edit ALL of the code here
 // function setup() {
-// const allEpisodes = getAllEpisodes();
-// makePageForEpisodes(allEpisodes);
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
 // }
-const allEpisodes = getAllEpisodes();
-console.log(allEpisodes);
+//You can edit ALL of the code here
+const search = document.getElementById("search");
+let results = document.getElementById("results");
+const selected = document.getElementById("selected");
+const selectInput = document.getElementById("select-episode");
+const dropdisplay = document.querySelector(".drop-display");
+const selectShow = document.getElementById("select-show");
+let option = document.getElementsByTagName("option");
+let term = "";
+
+const allShows = getAllShows();
+
+//Populate show dropdown select
+const populateShowsDropdown = allShows.forEach((show) => {
+  selectShow.innerHTML += `<option value="${show.id}">${show.name}</option>`;
+});
+
+// Funtion at the start of page load
 function onloadShow() {
-  allEpisodes.forEach((show) => {
-    //Zeropadded utility function
-    function zeroPadded(number) {
-      return number.toString().padStart(2, "0");
-    }
- 
+  allShows.forEach((show) => {
     if (show.image) {
       results.innerHTML += `
-                  <div class="show">
-                         <div class="show-preview">
-                          <h2>${show.name} - S${zeroPadded(
-        show.season
-      )}E${zeroPadded(show.number)}</h2>
-                         <img src="${show.image.medium}">
-                         <a href="#">View all Episodes <i class="fas fa-chevron-right"></i></a>
-                       </div>
-
-                        <div class="show-info">
-                        <h6>${show.name}</h6>
-                        ${show.summary}</div>
-                  </div>
-         `;
-    }
-  });
-}
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  console.log(episodeList);
-
-  function onloadShow() {
-    allShows.forEach((show) => {
-      if (show.image) {
-        results.innerHTML += `
                   <div class="show">
                          <div class="show-preview">
                          <h2>${show.name}</h2>
@@ -53,18 +38,117 @@ function makePageForEpisodes(episodeList) {
                         ${show.summary}</div>
                   </div>
          `;
-      }
-    });
-  }
+    }
+  });
 }
+//Call page on load function
+onloadShow();
+selectShow.addEventListener("change", (show) => {
+  if (show.target.value != "default") {
+    // results.style.display = "none";
+    let showValue = show.target.value;
+    console.log(showValue);
 
-window.onload = onloadShow;
+    //Load shows
 
-const search = document.createElement("input");
-search.type = "text";
-rootElem.appendChild(search);
-//Search box event listener
-search.addEventListener("keyup", (e) => {
-  term = e.target.value;
-  makePageForEpisodes(term);
+    fetch(`https://api.tvmaze.com/shows/${showValue}/episodes`)
+      .then((res) => res.json())
+      .then((episodes) => {
+        //Episodes function
+        function makePageForEpisodes() {
+          results.innerHTML = "";
+          selectInput.innerHTML = "";
+          episodes
+            .filter(
+              (episode) =>
+                episode.name.toLowerCase().includes(term.toLowerCase()) ||
+                episode.summary.toLowerCase().includes(term.toLowerCase())
+            )
+            .forEach((episode) => {
+              if (episode.image) {
+                results.innerHTML += `<div>
+   <h2>${episode.name} - S${zeroPadded(episode.season)}E${zeroPadded(
+                  episode.number
+                )}</h2>
+
+ <img src="${episode.image.medium}" >
+ ${episode.summary}
+  </div>
+     `;
+              }
+            });
+
+          //Searching message selected
+          const selectSearch = episodes.filter(
+            (episode) =>
+              episode.name.toLowerCase().includes(term.toLowerCase()) ||
+              episode.summary.toLowerCase().includes(term.toLowerCase())
+          );
+          if (term === "") {
+            selected.innerHTML = "";
+          } else {
+            selected.innerHTML = `${selectSearch.length} out of ${episodes.length} episode(s) selected.`;
+          }
+
+          //Populate the dropdownbox
+          const populateDropdown = episodes.forEach((episode) => {
+            let selectValue = `S${zeroPadded(episode.season)}E${zeroPadded(
+              episode.number
+            )} - ${episode.name}`;
+
+            selectInput.innerHTML += `<option value="${episode.name}">${selectValue}</option>`;
+          });
+
+          //Add drop down Event Listener
+          selectInput.addEventListener("change", (episode) => {
+            results.style.display = "none";
+            let dropValue = episode.target.value;
+            console.log(dropValue);
+            let count = 0;
+            if (typeof episodes != "undefined") {
+              const inputSelect = episodes.forEach((episode) => {
+                if (
+                  episode.name.toLowerCase().includes(dropValue.toLowerCase())
+                ) {
+                  results.innerHTML = `<div>
+                  <h2>${episode.name} - S${zeroPadded(
+                    episode.season
+                  )}E${zeroPadded(episode.number)}</h2>
+                <img src="${episode.image.medium}" >
+                ${episode.summary}
+                 </div>
+                    `;
+                  count++;
+                } else {
+                  results.style.display = "block";
+                }
+              });
+            }
+
+            selected.innerHTML = `${count} out of ${episodes.length} episode(s) selected.`;
+          });
+        }
+        // Load the page
+        makePageForEpisodes();
+
+        //Search box event listener
+        search.addEventListener("input", (e) => {
+          term = e.target.value;
+          makePageForEpisodes(term);
+        });
+      });
+  }
 });
+
+// fetchShowsApi();
+
+// const fetchEpisodes = async () => {
+//   episodes = await fetch(
+//     "https://api.tvmaze.com/shows/82/episodes"
+//   ).then((res) => res.json());
+// };
+
+//Zeropadded utility function
+function zeroPadded(number) {
+  return number.toString().padStart(2, "0");
+}
